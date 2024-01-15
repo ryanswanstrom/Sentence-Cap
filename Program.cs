9b35ba41e881534114066e788cd2ab43ae9bcc2c
Ryan Swanstrom <ryanswanstrom@users.noreply.github.com>
2024-01-15 16:09:31 +0000

    Pending changes exported from your codespace

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿
+namespace Coding
+{
+  public class Program
+  {
+    public static void Main()
+    {
+      string orig = "this is a sentence. do you like it? what a great day! let's go";
+      Console.WriteLine($"The original sentence [{orig}]");
+      string final = FixSentence(orig);
+      
+      Console.WriteLine($"The final sentence [{final}]");
+    }
+
+    public static String FixSentence(String original) 
+    {
+        char[] chars = original.ToCharArray();
+        bool beginOfSentence = true;
+        for (int i = 0; i < chars.Length; i++)
+        {
+            if (beginOfSentence && Char.IsLetter(chars[i]) )
+            {
+                chars[i] = Char.ToUpper( chars[i] );
+                beginOfSentence = false;
+            } 
+            else if (chars[i] == '!' || chars[i] == '.' || chars[i] == '?')
+            {                
+                beginOfSentence = true;
+            }
+        }
+
+        return new string(chars);
+    }
+  }
+}
